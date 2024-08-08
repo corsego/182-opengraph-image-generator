49a876e8f434524a0825cc09578126a5095490b6
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-08 23:02:55 +0200

    seed events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'open-uri'
+path = "https://raw.githubusercontent.com/ruby-conferences/ruby-conferences.github.io/main/_data/conferences.yml"
+uri = URI.open(path)
+yaml = YAML.load_file uri, permitted_classes: [Date]
+
+yaml.each do |event|
+  next if event["start_date"].to_datetime < Date.today
+
+  Event.create!(
+    name: event["name"],
+    location: event["location"],
+    start_date: event["start_date"],
+    url: event["url"],
+  )
+end
