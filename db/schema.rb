7a145aff325c4f716e88f19bbc5ec826df9fec08
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-08 23:02:03 +0200

    scaffold events

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_08_210131) do
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "start_date"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
